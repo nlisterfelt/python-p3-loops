01e94f31b2c97a844920521abd15ce8a19cf4c21
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-08-24 19:41:10 -0500

    while loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i=0
+    while i<=10:
+        print(10-i) if((10-i)!=0) else print("Happy New Year!")
+        i += 1
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list = [int**2 for int in int_list]
+    return int_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i=1
+    while i<=100:
+        if(i%3==0 and i%5==0):
+            print("FizzBuzz")
+        elif(i%3==0):
+            print('Fizz')
+        elif(i%5==0):
+            print('Buzz')
+        else:
+            print(i)
+        i += 1
